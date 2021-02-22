0a55ad232066dcf29fc76d35c4176f0deff300f0
cinna-codes <somethingfellipickeditup@gmail.com>
2021-02-22 18:09:45 -0500

    commit tree 3eecbdf7a91c60fc6ade22e84e1749f7678bff1d

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 = 2
+const num3 = 24
+const num4 = 5
 
-// Write your code here
+const multiply = num1 * num2 // is an equation whose multiplied results will equal 62
+
+const random = Math.floor(Math.random() * 10) + 1 // generates a random integer greater than 0
+
+const mod = num3 % num4 // is an equation that calculates a remainder that is equal to 4
+
+const max = Math.max(1,5,16,20) // will return 20 as the highest number in the set
